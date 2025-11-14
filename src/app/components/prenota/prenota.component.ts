650b5ec950ad9743525f18ea36a21a574e91577f
ilreleonecdc <ilreleonecdc@gmail.com>
2025-11-14 17:24:28 +0100

    fix: correct show date format in PrenotaComponent

--- src/app/components/prenota/prenota.component.ts
+++ src/app/components/prenota/prenota.component.ts
@@ -108,7 +108,7 @@ export class PrenotaComponent implements OnInit {
     {
       id: 'assisi-2026-03-21',
       title: 'Il Re Leone CDC – Il Musical',
-      date: '2026-03-xxT21:00:00+01:00',
+      date: '2026-03-21T21:00:00+01:00',
       venue: 'Teatro Lyrick',
       city: 'Assisi',
       billettoUrl: '',
